207fc2ca2a0adbd437686772518c1ac107300a35
Larry Liu <hrlarry@stanford.edu>
2014-01-22 17:01:27 -0800

    javascript lab

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -11,8 +11,56 @@ $(document).ready(function() {
 function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
+		$("#testjs").text("Test Javascript again");
+		$(".jumbotron p").toggleClass("active");
 	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+	$("a.thumbnail").click(projectClick);
+
+
+	$("#submitBtn").click(formClick);
+}
+
+function projectClick(e){
+	//prevent the page from reloading
+	console.log("Project clicked");
+	e.preventDefault();
+
+	$(this).css("background-color", "#7fff00");
+
+	var projectTitle = $(this).find("p").text();
+    var jumbotronHeader = $(".jumbotron h1");
+    jumbotronHeader.text(projectTitle);
+
+    var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+
+    if(description.length == 0){
+    	containingProject.append("<div class='project-description'><p>Description of the project.</p></div>");
+	} else {
+		//description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+		description.fadeOut();
+	}
+
+}
+
+function formClick(e){
+
+	console.log("Form submission");
+	e.preventDefault();
+
+	var ProjectChoice = $("#project").val();
+	var width = $("#width").val();
+
+	$(ProjectChoice).animate({
+		width: width
+	});
+
+	var descriptionText = $("#description").val();
+	console.log(descriptionText);
+	
+	$(ProjectChoice + " .project-description").text(descriptionText);
+
 }
